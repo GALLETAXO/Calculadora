a5a81ac1de4cafa9ab8bcf886a3521ab2f39269b
JoshVG2005 <guilltremp@gmail.com>
2024-11-24 18:44:53 -0600

    resta
    esto es la resta listo xd

---
+++ Calculadora/resta.h
@@ -0,0 +1,13 @@
+#ifndef RESTA_H
+#define RESTA_H
+
+class resta {
+private:
+    int Valor1, Valor2, resultado; // Variables privadas
+
+public:
+    resta();       // Constructor
+    void funcion(); // Método para realizar la resta
+};
+
+#endif
